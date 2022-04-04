dada0c0d010aefe71b48894b88eb5c994cf0293d
Павел Петров <petrov.pavel01@yandex.ru>
2022-04-05 02:05:43 +0300

    Добавьте файлы проекта.

---
+++ SLAPApp/SLAPApp/AddBoardPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SLAPApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddBoardPage : ContentPage
+    {
+        public AddBoardPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBackToMain_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ SLAPApp/SLAPApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SLAPApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonHome_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonAdd_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new AddBoardPage());
+        }
+
+        private void ButtonCalendar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
